8e8a1f5bc2e5093814aec136847cc56edf25208b
kacpersiedlarczyk <100533998+kacpersiedlarczyk@users.noreply.github.com>
2023-12-17 18:43:26 +0100

    allow to roll and hold dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import Die from "./Die";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [dice, setDice] = useState(allNewDice());
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function generateNewDie() {
+        return {
+            id: nanoid(),
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+        };
+    }
+
+    function allNewDice() {
+        const newDice = [];
+        for (let i = 0; i < 10; i++) {
+            newDice.push(generateNewDie());
+        }
+        return newDice;
+    }
+
+    function holdDice(id) {
+        setDice((prevDice) =>
+            prevDice.map((die) => {
+                if (die.id === id) {
+                    return { ...die, isHeld: !die.isHeld };
+                } else {
+                    return die;
+                }
+            })
+        );
+    }
+
+    function rollDice() {
+        setDice((prevDice) =>
+            prevDice.map((die) => (die.isHeld ? die : generateNewDie()))
+        );
+    }
+
+    const diceElements = dice.map((die) => {
+        return (
+            <Die
+                key={die.id}
+                value={die.value}
+                isHeld={die.isHeld}
+                holdDice={() => holdDice(die.id)}
+            />
+        );
+    });
+
+    return (
+        <main>
+            <div className="dice-container">{diceElements}</div>
+            <button className="roll-dice" onClick={rollDice}>
+                Roll Dice
+            </button>
+        </main>
+    );
 }
 
-export default App
+export default App;
